bb0a2f267f8c907a694d3c70e82bb2b74e0da8e8
HemanthRaghu96 <hemanthraghu96@gmail.com>
2025-11-29 23:49:16 +0530

    fix:updated

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,36 +3,38 @@ import React from "react";
 const Child = ({ isLoggedIn, setIsLoggedIn }) => {
   const [name, setName] = React.useState("");
   const [password, setPassword] = React.useState("");
+
   const handleSubmit = () => {
-    if (name && password) {
+    if (name.trim() !== "" && password.trim() !== "") {
       setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
     }
   };
+
   return (
     <>
       {!isLoggedIn && (
-        <form action="">
+        <form>
           <label htmlFor="name">Username:</label>
           <input
             type="text"
-            name="name"
             id="name"
             placeholder="Name"
             onChange={(e) => setName(e.target.value)}
           />
           <br />
+
           <label htmlFor="password">Password:</label>
           <input
             type="password"
-            name="password"
             id="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
           <br />
-          <input type="button" value="Login" onClick={handleSubmit} />
+
+          <button type="button" onClick={handleSubmit}>
+            Login
+          </button>
         </form>
       )}
     </>
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -5,13 +5,14 @@ const Parent = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   return (
-    <>
+    <div>
       <div>Parent Component</div>
 
       {isLoggedIn && <div>You are logged in!</div>}
 
+      {/* Passing state and setter to child */}
       <Child isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-    </>
+    </div>
   );
 };
 
