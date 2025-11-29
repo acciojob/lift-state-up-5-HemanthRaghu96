632c41c523786ace4e957d1057c4faf4127fb799
HemanthRaghu96 <hemanthraghu96@gmail.com>
2025-11-29 22:57:41 +0530

    fix: updated

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
-const Child = ({ isLoggedIn }) => {
-  return <>{isLoggedIn && <div>You are logged in!</div>}</>;
+const Child = ({ isLoggedIn, setIsLoggedIn }) => {
+  const [name, setName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const handleSubmit = () => {
+    if (name && password) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
+  return (
+    <>
+      {!isLoggedIn && (
+        <form action="">
+          <label htmlFor="name">Username:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input type="button" value="Login" onClick={handleSubmit} />
+        </form>
+      )}
+    </>
+  );
 };
 
 export default Child;
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -3,44 +3,14 @@ import Child from "./Child";
 
 const Parent = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [name, setName] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
-  const handleSubmit = () => {
-    if (name && password) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  };
   return (
     <>
       <div>Parent Component</div>
-      {!isLoggedIn && (
-        <form action="">
-          <label htmlFor="name">Username:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <input type="button" value="Login" onClick={handleSubmit} />
-        </form>
-      )}
 
-      <Child isLoggedIn={isLoggedIn} />
+      {isLoggedIn && <div>You are logged in!</div>}
+
+      <Child isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
     </>
   );
 };
